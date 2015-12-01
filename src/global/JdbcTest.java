0e97be8c2753ecc0c8486c59051b6f0c8895034e
skysoblue <coolbeat@naver.com>
2015-12-01 16:02:26 +0900

    .

--- src/global/JdbcTest.java
+++ src/global/JdbcTest.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcTest {
-	private Connection connection;
+	private static Connection connection;
 	private static JdbcTest instance = new JdbcTest();
 	private JdbcTest() {
 		try {
@@ -29,22 +29,30 @@ public class JdbcTest {
 	}
 
 	public static void main(String[] args) {
+        PreparedStatement pstmt = null;
         Statement stmt = null;
         ResultSet rs = null;
         try {
-            
-				stmt = JdbcTest.getInstance().getConnection()
-						.createStatement();
-				rs = stmt.executeQuery("select * from member where userid = 'hong'");
-				String temp = null;
-				while (rs.next()) {
-					temp = rs.getString("name");
-				}
-				System.out.println("임시값 : "+temp);
+    		String sql = "SELECT * FROM TEST";
+    		connection = JdbcTest
+    				.getInstance()
+    				.getConnection();
+    		if(connection == null){
+				System.out.println("Connection 이 널");
+			} else {
+				stmt = connection.createStatement();
+			}
+			rs = stmt.executeQuery(sql);
+			String temp = null;
+			while (rs.next()) {
+				temp = rs.getString("TEST");
+			}
+			System.out.println("임시값 : "+temp);
         } catch (Exception e ) {
             System.out.println("HSQLDB 에러발생 !!");
             e.printStackTrace();
             return;
         } 
 	}
+	
 }
